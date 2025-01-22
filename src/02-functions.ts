92cff95112cef6a3d5b5f72afed458a31ab8606e
tmolnar <tibor.molnar@waltoninstitute.ie>
2025-01-22 22:51:57 +0000

    pushing to remote

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,2 +1,20 @@
 import {Friend, Colleague } from './myTypes'
 import { friends, colleagues } from "./01-basics";
+
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]) : string[] {
+    return friends.map(friend =>{
+        friend.age += 1;
+        return `${friend.name} is now ${friend.age}`
+    });
+
+}
+
+console.log(allOlder(friends))
