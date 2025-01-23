085f8cb78b47fb4da0ff71f4825e7016ddb243d1
tmolnar <tibor.molnar@waltoninstitute.ie>
2025-01-23 21:52:58 +0000

    Typed function signature

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
 import { friends, colleagues } from "./01-basics";
 
 
@@ -18,3 +18,33 @@ function allOlder(friends: Friend[]) : string[] {
 }
 
 console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+  
+  function addColleague(currentColleagues : Colleague[], name : string, department : string, email: string) {
+    
+    const newColleague: Colleague = {
+        name: name,
+        department: department,
+        contact: {
+          email: email,
+          extension: 133,
+        }
+      };
+      
+      currentColleagues.push(newColleague)
+  };
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+// console.log(colleagues.current)
+
+
